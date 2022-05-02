345295bdaebc2745f6ccdb723e2395d809b0cd12
miaamitchell <mmitchell601@gmail.com>
2022-05-02 17:19:13 -0500

    signupchanges
    deleted aboutus page & worked on the sign up page

--- EagleEatsMaster/AboutUS.aspx
+++
@@ -1,6 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EagleEats.Master" AutoEventWireup="true" CodeBehind="AboutUs.aspx.cs" Inherits="EagleEatsMaster.AboutUS" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>About Us</h1>
-</asp:Content>
--- EagleEatsMaster/SignUp.aspx.cs
+++ EagleEatsMaster/SignUp.aspx.cs
@@ -7,11 +7,49 @@ using System.Web.UI.WebControls;
 
 namespace EagleEatsMaster
 {
-    public partial class CustomerSignUp : System.Web.UI.Page
+    public partial class SignUp : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected void btnSignUp_Click(object sender, EventArgs e)
+        {
+            if (!Page.IsValid)
+                return;
+            //step 1 prepare all parameters
+            var parameters = this.SqlDataSource1.InsertParameters;
+            parameters["UserId"].DefaultValue = tbUser.Text;
+            parameters["Password"].DefaultValue = tbPassword.Text;
+            parameters["Address"].DefaultValue = tbAddress.Text;
+
+            try
+            {
+                //step 2 insert into db
+                this.SqlDataSource1.Insert();
+
+                //step 3 reset the controls
+                resetControls();
+
+                //step 4 redirect to itself to avoid resubmit the insert action
+                Response.Redirect("SignUp.aspx");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+            protected void btnReset_Click(object sender, EventArgs e)
+            {
+                resetControls();
+            }
+            private void resetControls()
+            {
+                tbUser.Text = "";
+                tbPassword.Text = "";
+                tbAddress.Text = "";
+            }
     }
 }
