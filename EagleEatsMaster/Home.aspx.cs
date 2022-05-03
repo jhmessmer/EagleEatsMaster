ba875ee3e643d04cd3008ea85ee1981c3566f574
miaamitchell <mmitchell601@gmail.com>
2022-05-02 19:03:15 -0500

    some login code

--- EagleEatsMaster/Home.aspx.cs
+++ EagleEatsMaster/Home.aspx.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Windows.Forms;
 
 namespace EagleEatsMaster
 {
@@ -16,7 +20,23 @@ namespace EagleEatsMaster
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            
+            string Username = tbUser.Text;
+            string Password = tbPassword.Text;
+            int loggedIntUser ID;
+
+            string constring = ConfigurationManager.ConnectionString["EagleEastConnectionString"].ConnectionString;
+            string query = "SELECT [Id], [Username], [Password] FROM [User] WHERE (([Username] = @Username) AND ([Password] = @Password));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            { 
+                System.Diagnostics.Process.Start("");
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
         }
     }
 }
