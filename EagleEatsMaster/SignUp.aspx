51dc1f8b8ee0a81395eed10e6f2627d9282d1da4
miaamitchell <mmitchell601@gmail.com>
2022-05-05 01:06:10 -0500

    textboxes and labels

--- EagleEatsMaster/SignUp.aspx
+++ EagleEatsMaster/SignUp.aspx
@@ -1,33 +1,9 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SignUp.aspx.cs" Inherits="EagleEatsMaster.SignUp" %>
-
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-    <style type="text/css">
-.h1,h1{font-size:36px}.h1,.h2,.h3,h1,h2,h3{margin-top:20px;margin-bottom:10px}.h1,.h2,.h3,.h4,.h5,.h6,h1,h2,h3,h4,h5,h6{font-family:inherit;font-weight:500;line-height:1.1;color:inherit}h1{font-size:2em;margin:.67em 0}*{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}*,:after,:before{color:#000!important;text-shadow:none!important;background:0 0!important;-webkit-box-shadow:none!important;box-shadow:none!important}.h2,h2{font-size:30px}h2,h3{page-break-after:avoid}h2,h3,p{orphans:3;widows:3}button,input,select,textarea{font-family:inherit;font-size:inherit;line-height:inherit}input{line-height:normal}button,input,optgroup,select,textarea{color:inherit;font:inherit;margin:0}button,html input[type=button],input[type=reset],input[type=submit]{-webkit-appearance:button;cursor:pointer}</style>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EagleEats.Master" AutoEventWireup="true" CodeBehind="SignUp.aspx.cs" Inherits="EagleEatsMaster.CustomerSignUp" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1>Customer Sign Up</h1>
     <h2>Please enter the following information to register for EagleEats deliveries!</h2>
-            <p>
-                <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:EagleEatsDBConnectionString %>" InsertCommand="INSERT INTO [User] (UserName, Passwd, Address) VALUES (@UserName, @Passwd, @Address)" SelectCommand="SELECT * FROM [User]" UpdateCommand="UPDATE [User] SET UserName = @UserName, Passwd = @Passwd, Address= @Address WHERE (User_Id = @User_Id)">
-                    <InsertParameters>
-                        <asp:Parameter Name="UserName" />
-                        <asp:Parameter Name="Passwd" />
-                        <asp:Parameter Name="Address" />
-                    </InsertParameters>
-                    <UpdateParameters>
-                        <asp:Parameter Name="UserName" />
-                        <asp:Parameter Name="Passwd" />
-                        <asp:Parameter Name="Address" />
-                        <asp:Parameter Name="User_Id" />
-                    </UpdateParameters>
-                </asp:SqlDataSource>
-            </p>
     <asp:Label ID="Label1" runat="server" Text="Username: "></asp:Label>
     <asp:TextBox ID="tbUser" runat="server"></asp:TextBox>
     <br />
@@ -39,11 +15,9 @@
     <br />
     <asp:Label ID="Label4" runat="server" Text="Address: "></asp:Label>
     <asp:TextBox ID="tbAddress" runat="server"></asp:TextBox>
-    <br>
+    <br />
     <br />
     <asp:Button ID="btnSignUp" runat="server" Text="Sign Up" />
     <asp:Button ID="btnCancel" runat="server" PostBackUrl="~/Home.aspx" Text="Cancel" />
-        </div>
-    </form>
-</body>
-</html>
+    <h1>&nbsp;</h1>
+</asp:Content>
--- EagleEatsMaster/SignUp.aspx.cs
+++ EagleEatsMaster/SignUp.aspx.cs
@@ -1,136 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Windows.Forms;
 
 namespace EagleEatsMaster
 {
-    public partial class SignUp : System.Web.UI.Page
+    public partial class CustomerSignUp : System.Web.UI.Page
     {
-
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        SqlConnection con;
-        SqlCommand cmd;
-        SqlDataReader reader;
-
-        protected void btnSignUp_Click(object sender, EventArgs e)
-        {
-            if (!Page.IsValid)
-                return;
-            var parameters = this.SqlDataSource1.InsertParameters;
-            parameters["UserName"].DefaultValue = tbUser.Text;
-            parameters["Password"].DefaultValue = tbPassword.Text; 
-            parameters["Address"].DefaultValue = tbAddress.Text;
-
-            try
-            {
-                this.SqlDataSource1.Insert();
-
-                Response.Redirect("Home.aspx");
-
-            }
-            catch (Exception)
-            {
-                
-            }
-
-           // SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=EagleEatsDB;Integrated Security=True");
-           // {
-           //     con.Open();
-           //     SqlCommand cmd = new SqlCommand("INSERT into User values(@UserName, @Passwd, @Address)", con);
-           //     cmd.Parameters.AddWithValue("UserName", tbUser.Text);
-           //     cmd.Parameters.AddWithValue("Passwd", tbPassword.Text);
-           //     cmd.Parameters.AddWithValue("Address", tbAddress.Text);
-           //     cmd.ExecuteNonQuery();
-           // }
-            /*
-            string Username = tbUser.Text;
-            string Password = tbPassword.Text;
-            string Confirm = tbConfirm.Text;
-            string Address = tbAddress.Text;
-
-            string constring = ConfigurationManager.ConnectionStrings["EagleEatsDBConnectionString"].ConnectionString;
-            string query = "INSERT into [User] values(@UserName,@Passwd,@Address)";
-
-            /*
-            using (var con = new SqlConnection(constring));
-            {
-                var command = new SqlCommand(query, con);
-                command.Parameters.AddWithValue("@UserName", Username);
-                command.Parameters.AddWithValue("@Passwd", Password);
-                command.Parameters.AddWithValue("@Address", Address);
-
-                try
-                {
-                    con.Open();
-                    using (var reader = command.ExecuteReader(CommandBehavior.CloseConnection))
-                    {
-                        if(reader.Read())
-                        {
-                            Session["User_Id"] = Convert.ToInt32(reader["Id"]);
-                            Session["Passwd"] = Password;
-                            Response.Redirect("Home.aspx");
-                        }
-                    }
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Error");
-                }
-            }
-            
-
-            
-            using (var con = new SqlConnection(constring))
-            {                
-
-                var command = new SqlCommand(query, con);
-                if (Confirm != string.Empty || Password != string.Empty || Username != string.Empty || Address != string.Empty)
-                {
-                    if (Password == Confirm)
-                    {
-                        cmd = new SqlCommand("SELECT * from [User] WHERE UserName='" + Username + "'", con);
-                        var reader = command.ExecuteReader();
-                        if (reader.Read())
-                        {
-                            con.Close();
-                            MessageBox.Show("Username already exists, please try another ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        }
-                        else
-                        {
-                            con.Close();
-                            cmd = new SqlCommand("INSERT into [User] values(@UserName,@Passwd,@Address)", con);
-                            command.Parameters.AddWithValue("@UserName", Username);
-                            command.Parameters.AddWithValue("@Passwd", Password);
-                            command.Parameters.AddWithValue("@Address", Address);
-                            command.ExecuteNonQuery();
-                            MessageBox.Show("Account successfully created. Please login now.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            Response.Redirect("Home.aspx");
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Please ensure passwords match.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Please fill all fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            
-           }
-            */
-           
-        }
-        }
+    }
 }
